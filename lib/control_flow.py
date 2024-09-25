725e61713de308d3b3152eddef9d8efdd27b415d
daniel nzioka <dmwinzi50@gmail.com>
2024-09-25 09:11:00 +0300

    conditional statement

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,62 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    # username=("ADMIN","admin")
+    # password="12345"
+    # return "Access granted" if username="true" and password="true" else "Access denied"
+    if(username =="admin" and password =="12345"):
+        return "Access granted"
+    elif(username =="ADMIN" and password =="12345"):
+        return "Access granted"
+    else:
+        return "Access denied"
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    # temperature== <40
+    # dict_map={
+    #     >40 and <65 : "It's a little chilly out there!",
+    #     >85 : "It's too dang hot out there!",}
+    # weather =dict_map.get(temperature,"It's perfect out there!")
+    # return weather
+    if(temperature  < 40):
+        return "It's brisk out there!"
+    elif(temperature  >= 40 and temperature <= 65):
+        return "It's a little chilly out there!"
+    elif(temperature  > 85):
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+
 
 def fizzbuzz(num):
+    if(num % 3 == 0 and num % 5 == 0):
+        return "FizzBuzz"
+    elif(num % 5 == 0):
+        return "Buzz"
+    elif(num % 3 == 0):
+        return "Fizz"
+    else:
+        return num
     # your code here
-    pass
 
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    # operation=["+","-","*","/"]
+    # if(operation[i] == "true"):
+    #     return (num1 operation[i] num2)
+    # else:
+    #     print("Invalid operation!")
+    #     return None
+    if(operation == "+"):
+        return num1 + num2
+    elif(operation == "-"):
+        return num1 - num2
+    elif(operation == "*"):
+        return num1 * num2
+    elif(operation == "/"):
+        return num1 / num2
+    else:
+        print("Invalid operation!")
+        return None
+               
